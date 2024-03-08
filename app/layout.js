b3e2a59f79394479e725027a53cb1d23e5363c9d
amzxir <amirreza79ahmadi@gmail.com>
2024-03-08 22:45:52 +0330

    add font next 14

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,21 @@
 import "./globals.css";
+import localFont from "next/font/local";
 
 export const metadata = {
   title: "next js 14",
   description: "next js 14 cache course",
 };
 
+const vazirFont = localFont({
+  src: "../public/font/Vazirmatn-Regular.woff2",
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`bg-slate-900 text-slate-200 ${vazirFont.className}`}>
+        <main className="p-5">{children}</main>
+      </body>
     </html>
   );
 }
